8bd69ab20f479a1a6dcd1996c290818422b153dc
ChidiebereMicah <chidieberemicah@gmail.com>
2026-09-22 16:06:02 +0100

    Data Model Mini Project

---
+++ lessonExamples/Classes and OOP/data_model_practice/dataModelImplementation.py
@@ -0,0 +1,30 @@
+from sklearn.datasets import load_iris
+
+"""
+Build a class called:
+Dataset
+
+It represents a small collection of training samples.
+Each sample is a dictionary:
+For example:
+samples = [
+    {"text": "Python is great", "label": "positive"},
+    {"text": "I dislike bugs", "label": "negative"},
+    {"text": "Learning is fun", "label": "positive"},
+]
+"""
+
+iris = load_iris()
+print(iris)
+training_samples = [
+    
+]
+
+
+"""
+Your Dataset class should support the following Python behaviors.
+
+A. Length
+This should work:
+len(dataset)
+"""

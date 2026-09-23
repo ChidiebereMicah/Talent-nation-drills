f083581b0da17b39d0b42a91ec6b3d8b757b7c68
ChidiebereMicah <chidieberemicah@gmail.com>
2026-09-23 22:01:21 +0100

    Update DataModelImplementation

--- lessonExamples/Classes and OOP/data_model_practice/dataModelImplementation.py
+++ lessonExamples/Classes and OOP/data_model_practice/dataModelImplementation.py
@@ -21,13 +21,18 @@ iris = load_iris()
 # print(iris["data"].shape)
 # print(iris["data"][0])
 
-print(iris["feature_names"])
-print(iris["target"][0])
-print(iris["target_names"])
+# print(iris["feature_names"])
+# print(iris["target"][0])
+# print(iris["target_names"])
 training_samples = [
     
 ]
 
+features = [feature for feature in iris["feature_names"]]
+specs = [spec for spec in iris["data"][0]]
+flower_0 = {features[i]: specs[i] for i in range(len(features))}
+flower_0["species"] = iris["target_names"][0]
+print(flower_0)
 
 """
 Your Dataset class should support the following Python behaviors.

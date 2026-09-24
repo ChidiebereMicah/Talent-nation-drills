e512e67f5de683ff81245a126f90f13aa9a0913f
ChidiebereMicah <chidieberemicah@gmail.com>
2026-09-24 15:45:16 +0100

    Update dataModelImplementation

--- lessonExamples/Classes and OOP/data_model_practice/dataModelImplementation.py
+++ lessonExamples/Classes and OOP/data_model_practice/dataModelImplementation.py
@@ -15,6 +15,10 @@ samples = [
 """
 
 iris = load_iris()
+"""
+    The iris dataset is obtained from the UCI Machine Learning Repository
+    Its a dataset of flowers and their features
+"""
 # print(iris)
 # print(type(iris))
 # print(type(iris["data"]))
@@ -24,16 +28,27 @@ iris = load_iris()
 # print(iris["feature_names"])
 # print(iris["target"][0])
 # print(iris["target_names"])
-training_samples = [
-    
-]
 
-features = [feature for feature in iris["feature_names"]]
-specs = [spec for spec in iris["data"][0]]
-flower_0 = {features[i]: specs[i] for i in range(len(features))}
-flower_0["species"] = iris["target_names"][0]
-print(flower_0)
+#to build a single flower
+# features = [iris["feature_names"]
+# specs = iris["data"][0]
+# flower_0 = {features[i]: specs[i] for i in range(len(features))}
+# flower_0["species"] = iris["target_names"][0]
+# print(flower_0)
 
+#helper fn to build any flower for any given index
+def make_flower(iris, index):
+    flower = {feature.replace(' (cm)', '').replace(' ', '_'):float(value) for feature, value in zip(iris['feature_names'], iris['data'][index])}
+    flower['species'] = str(iris['target_names'][iris['target'][index]])
+    return flower
+
+print(flower_0 := make_flower(iris, 0))
+print(flower_50 := make_flower(iris, 50))
+print(flower_100 := make_flower(iris, 100))
+
+#Building the full dataset from iris
+samples = [make_flower(iris, index) for index in range(len(iris['data']))]
+print(samples)
 """
 Your Dataset class should support the following Python behaviors.
 
@@ -41,3 +56,9 @@ A. Length
 This should work:
 len(dataset)
 """
+class Dataset:
+    def __init__(self, samples):
+        self.samples = samples
+
+    def __len__(self, samples):
+
